603a1b6b7ee95761a84d55177a41f185574eac60
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-12 00:50:29 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,4 @@
 def mario
-  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts phrase = "It's-a me, Mario!"
   phrase
 end
@@ -13,3 +12,7 @@ def link
   puts phrase = "It's Dangerous To Go Alone! Take This."
   phrase
 end
+
+def any_phrase
+  puts 
+  
